a1eb4fb86309d587e8b37c387630772d01f08a7d
yaron2 <schneider.yaron@live.com>
2018-05-14 12:25:20 -0700

    gitignore and dist fix

--- cli/build_cli.sh
+++ cli/build_cli.sh
@@ -10,6 +10,6 @@ gox -osarch="linux/amd64" -output "dist/linux/legion"
 gox -osarch="darwin/amd64" -output "dist/darwin/legion"
 gox -osarch="windows/amd64" -output "dist/windows/legion"
 
-tar -cvf legion_"$VERSION"_linux_amd64.tar dist/linux/legion
-tar -cvf legion_"$VERSION"_darwin_amd64.tar dist/darwin/legion
-tar -cvf legion_"$VERSION"_windows_amd64.tar dist/windows/legion.exe
+tar -cvf legion_"$VERSION"_linux_amd64.tar -C ./dist/linux/ legion
+tar -cvf legion_"$VERSION"_darwin_amd64.tar -C ./dist/darwin/ legion
+tar -cvf legion_"$VERSION"_windows_amd64.tar -C ./dist/windows/ legion.exe
