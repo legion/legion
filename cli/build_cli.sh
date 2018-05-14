ec00dcc5a18d42c03174939118db5401c6e4fa82
yaron2 <schneider.yaron@live.com>
2018-05-14 11:57:02 -0700

    build cli fix

--- cli/build_cli.sh
+++ cli/build_cli.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION="0.0.1"
+
 mkdir -p dist/linux
 mkdir -p dist/darwin
 mkdir -p dist/windows
@@ -7,3 +9,7 @@ mkdir -p dist/windows
 gox -osarch="linux/amd64" -output "dist/linux/legion"
 gox -osarch="darwin/amd64" -output "dist/darwin/legion"
 gox -osarch="windows/amd64" -output "dist/windows/legion"
+
+tar -cvf dist/linux/legion_"$VERSION"_linux_amd64.tar dist/linux/legion
+tar -cvf dist/darwin/legion_"$VERSION"_darwin_amd64.tar dist/darwin/legion
+tar -cvf dist/windows/legion_"$VERSION"_windows_amd64.tar dist/windows/legion.exe
